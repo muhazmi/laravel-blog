905ba55def280aa93778f1d49fddfe1169797a6f
MuhAzmi <azmi2793@gmail.com>
2023-09-10 10:01:55 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,5 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('/category', CategoryController::class);
