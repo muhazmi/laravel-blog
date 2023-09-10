0ca2a5380301fb907a44e588f1906bcf1ec76f22
MuhAzmi <azmi2793@gmail.com>
2023-09-10 20:35:40 +0700

    add post, home and category view

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -1,5 +1,22 @@
 @extends('front.layout.app')
 
 @section('content')
-  <h1>This is The Content Section!</h1>
+  <h1>Latest Blog</h1>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      @foreach ($posts as $post)
+        <div class="col">
+          <div class="card">
+            <img src="{{ $post->image }}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ Str::limit($post->name, 35) }}</h5>
+              <p class="card-text">{{ Str::limit($post->description, 75) }}</p>
+            </div>
+            <div class="card-footer">
+              <a href="post/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
 @endsection
