1b196d8c227f7a8a19699f2fa1178c7a5645ec90
MuhAzmi <azmi2793@gmail.com>
2023-09-10 10:01:48 +0700

    update category controller, and view

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $data = [
+            'page_title' => 'Category'
+        ];
+        return view('front/category/index', $data);
     }
 
     /**
--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -5,9 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>{{ $page_title }}</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
   
 </body>
 </html>
-<h1>Ini {{ $page_title }}</h1>
+<h1>Ini halaman {{ $page_title }}</h1>
