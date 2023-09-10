ba33a9d22aae09566a132ec6de68b2d3e2cbf578
MuhAzmi <azmi2793@gmail.com>
2023-09-10 09:33:47 +0700

    add home controller, routes, and index view

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'page_title' => 'Home'
+        ];
+        return view('front/home/index', $data);
+    }
+}
