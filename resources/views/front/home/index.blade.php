fdbe894871bebb8cdd4f0789b78917902341321c
MuhAzmi <azmi2793@gmail.com>
2023-09-10 10:47:53 +0700

    update homepage view

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $page_title }}</title>
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-  
-</body>
-</html>
-<h1>Ini halaman {{ $page_title }}</h1>
+@extends('front.layout.app')
+
+@section('content')
+  <h1>This is The Content Section!</h1>
+@endsection
