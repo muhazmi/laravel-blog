62ee7ca6f6f2c0ee6f407461bdb1eae1a84ca5b7
MuhAzmi <azmi2793@gmail.com>
2023-09-10 20:32:58 +0700

    add post model and pull data from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,7 +10,8 @@ class HomeController extends Controller
     public function index()
     {
         $data = [
-            'page_title' => 'Home'
+            'page_title' => 'Home',
+            'posts' => Post::latest()->limit(9)->get()
         ];
         return view('front/home/index', $data);
     }
